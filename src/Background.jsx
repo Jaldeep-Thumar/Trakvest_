7093801d07bf021c0b0069e4b7b870803695a24a
Jaldeep Thumar <jaldeept81@gmail.com>
2025-09-19 12:06:08 +0530

    fix

--- src/Background.jsx
+++ src/Background.jsx
@@ -7,7 +7,7 @@ function Background() {
       <div className="bg-gray-950/10 top-0 h-16 w-full"></div>
       <div className="relative w-full h-screen">
         <img
-          src="./src/assets/gifs2.gif"
+          src="assets/gifs2.gif"
           alt="gif"
           className="w-full h-full object-cover opacity-90"
         />
